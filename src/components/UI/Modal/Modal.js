eddc5439f9c18fb3ba80c40b351296298312fcad
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-22 10:41:10 -0500

    Showing & Hiding the Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,7 +3,12 @@ import React from 'react';
 import classes from './Modal.css';
 
 const modal = (props) => (
-    <div className={classes.Modal}>
+    <div
+        style={{
+            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: props.show ? '1' : '0'
+        }}
+        className={classes.Modal}>
         {props.children}
     </div>
 );
